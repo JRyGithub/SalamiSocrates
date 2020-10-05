d363105c376610c32715f6974c3808588346b4ed
Josh Ryland <jryland@protonmail.com>
2020-10-05 21:05:02 +1300

    Altering little done

--- salami-socrates/src/App.js
+++ salami-socrates/src/App.js
@@ -3,22 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        {images.map(i => <img src={i} alt="" />)}
+      </section>
     </div>
   );
 }
